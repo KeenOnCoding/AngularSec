8f3d1fdc750e802c4706a5befec18a267d2d6558
Home <none@gmail.com>
2023-05-11 10:57:43 +0300

    Initial

---
+++ src/server/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AngularSec.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
---
+++ src/server/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AngularSec.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
